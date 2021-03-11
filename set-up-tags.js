c8d89e3449c4e36d5eeb71ae1709e6e604a693da
tomaszew95 <50740828+tomaszew95@users.noreply.github.com>
2021-03-11 07:51:23 +0100

    test

--- set-up-tags.js
+++ set-up-tags.js
@@ -2,22 +2,41 @@ var tagsToClassses = document.getElementById("ceros-tags-to-classes-plugin");
 var objectsNames = tagsToClassses.getAttribute("objects-names").split(" ");
 var objects = new Array();
 (function(){
-    'use strict';
-    require.config({
-        paths: {
-            CerosSDK: '//sdk.ceros.com/standalone-player-sdk-v5.min'
-        }
-    });
-    require(['CerosSDK'], function (CerosSDK) {
-        CerosSDK.findExperience()
-            .fail(function (error) {
-                console.error(error);
-            })
-            .done(function (experience) {
+    'use strict'; 
+    require.config({ 
+        paths: { 
+            CerosSDK: '//sdk.ceros.com/standalone-player-sdk-v5.min' 
+        } 
+    }); 
+    require(['CerosSDK'], function (CerosSDK) { 
+        CerosSDK.findExperience() 
+            .fail(function (error) { 
+                console.error(error); 
+            }) 
+            .done(function (experience) { 
                 window.myExperience = experience;
 
                 experience.on(CerosSDK.EVENTS.PAGE_CHANGED, pageChangedCallback);
                 function pageChangedCallback(){
+                    console.log("works1");
+                    if(objectsNames.length > 0){
+                        for(let i = 0; i<objectsNames.length; i++){
+                            switch(true){
+                                case (experience.findLayersByTag(objectsNames[i]).layers.length > 0):
+                                    objects.push(experience.findLayersByTag(objectsNames[i]));
+                                    break;
+                                case (experience.findSyncedObjectsByTag(objectsNames[i]).syncedObjects.length > 0):
+                                    objects.push(experience.findSyncedObjectsByTag(objectsNames[i]));
+                                    break;
+                                default:
+                                    console.error("undefined tag name: " + objectsNames[i]);
+                            }
+                        }
+                    }
+                    else{
+                        console.error("array 'objectNames' is empty");
+                    }
+
                     console.log("works2");
                     for(let y = 0; y<objects.length; y++){
                         objects[y].layers.forEach(function(component){
@@ -26,27 +45,6 @@ var objects = new Array();
                         }); 
                     }
                 }
-            })
-    });
+            }) 
+    }); 
 })();
-
-function initialProcess(){
-    console.log("works1");
-    if(objectsNames.length > 0){
-        for(let i = 0; i<objectsNames.length; i++){
-            switch(true){
-                case (experience.findLayersByTag(objectsNames[i]).layers.length > 0):
-                    objects.push(experience.findLayersByTag(objectsNames[i]));
-                    break;
-                case (experience.findSyncedObjectsByTag(objectsNames[i]).syncedObjects.length > 0):
-                    objects.push(experience.findSyncedObjectsByTag(objectsNames[i]));
-                    break;
-                default:
-                    console.error("undefined tag name: " + objectsNames[i]);
-            }
-        }
-    }
-    else{
-        console.error("array 'objectNames' is empty");
-    }
-};
